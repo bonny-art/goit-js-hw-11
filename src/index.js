35ef180a42cace60605f1b3c9ff556b94d854ff6
bonny-art <svitlana.otenko@gmail.com>
2023-11-02 16:58:31 +0200

    adds infinity scroll

--- src/index.js
+++ src/index.js
@@ -6,16 +6,108 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-el.form.addEventListener('submit', onSearch);
-el.moreButton.addEventListener('click', onLoadMore);
+// ======================== Button Load more ========================
 
-let page = 1;
+// el.form.addEventListener('submit', onSearch);
+// el.moreButton.addEventListener('click', onLoadMore);
+
+// let page = 1;
+// let query = '';
+// let instanceSimpleLightbox;
+
+// function onSearch(e) {
+//   e.preventDefault();
+//   el.moreButton.classList.add('load-more-hidden');
+
+//   const searchQuery = e.target.elements.searchQuery.value.trim();
+//   if (searchQuery === '') return;
+//   query = searchQuery;
+
+//   getPhotos(searchQuery)
+//     .then(({ data }) => {
+//       if (data.hits.length === 0) {
+//         Notify.failure(
+//           'Sorry, there are no images matching your search query. Please try again.'
+//         );
+//         return;
+//       }
+
+//       Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+//       const markup = createMarkup(data);
+//       el.gallery.innerHTML = markup;
+//       instanceSimpleLightbox = new SimpleLightbox('.gallery-card', {});
+
+//       if (data.totalHits > PER_PAGE) {
+//         el.moreButton.classList.remove('load-more-hidden');
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       Notify.failure(
+//         `Sorry, something went wrong. Error ${err}. Please try again.`
+//       );
+//     });
+// }
+
+// function onLoadMore() {
+//   page += 1;
+
+//   const searchQuery = query;
+
+//   getPhotos(searchQuery, page)
+//     .then(({ data }) => {
+//       const markup = createMarkup(data);
+//       el.gallery.insertAdjacentHTML('beforeend', markup);
+//       instanceSimpleLightbox.refresh();
+
+//       const { height: cardHeight } = document
+//         .querySelector('.gallery')
+//         .firstElementChild.getBoundingClientRect();
+
+//       window.scrollBy({
+//         top: cardHeight * 2,
+//         behavior: 'smooth',
+//       });
+
+//       if (page * PER_PAGE >= data.totalHits) {
+//         el.moreButton.classList.add('load-more-hidden');
+//         Notify.failure(
+//           "We're sorry, but you've reached the end of search results."
+//         );
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       Notify.failure(
+//         `Sorry, something went wrong. Error ${err}. Please try again.`
+//       );
+//     });
+// }
+
+// ======================== Intersection observer ========================
+
+el.form.addEventListener('submit', onSearch);
+
+const options = {
+  root: null,
+  rootMargin: '600px',
+};
+
+const observer = new IntersectionObserver(onLoadMore, options);
+
+let page;
 let query = '';
 let instanceSimpleLightbox;
 
 function onSearch(e) {
   e.preventDefault();
-  el.moreButton.classList.add('load-more-hidden');
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+
+  page = 1;
 
   const searchQuery = e.target.elements.searchQuery.value.trim();
   if (searchQuery === '') return;
@@ -37,7 +129,7 @@ function onSearch(e) {
       instanceSimpleLightbox = new SimpleLightbox('.gallery-card', {});
 
       if (data.totalHits > PER_PAGE) {
-        el.moreButton.classList.remove('load-more-hidden');
+        observer.observe(el.guard);
       }
     })
     .catch(err => {
@@ -48,37 +140,47 @@ function onSearch(e) {
     });
 }
 
-function onLoadMore() {
-  page += 1;
+function onLoadMore(enteries, observer) {
+  enteries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
 
-  const searchQuery = query;
+      const searchQuery = query;
 
-  getPhotos(searchQuery, page)
-    .then(({ data }) => {
-      const markup = createMarkup(data);
-      el.gallery.insertAdjacentHTML('beforeend', markup);
-      instanceSimpleLightbox.refresh();
+      getPhotos(searchQuery, page)
+        .then(({ data }) => {
+          const markup = createMarkup(data);
+          el.gallery.insertAdjacentHTML('beforeend', markup);
+          instanceSimpleLightbox.refresh();
 
-      const { height: cardHeight } = document
-        .querySelector('.gallery')
-        .firstElementChild.getBoundingClientRect();
-
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
-      });
-
-      if (page * PER_PAGE >= data.totalHits) {
-        el.moreButton.classList.add('load-more-hidden');
-        Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      Notify.failure(
-        `Sorry, something went wrong. Error ${err}. Please try again.`
-      );
-    });
+          if (page * PER_PAGE >= data.totalHits) {
+            observer.unobserve(el.guard);
+            Notify.failure(
+              "We're sorry, but you've reached the end of search results."
+            );
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          Notify.failure(
+            `Sorry, something went wrong. Error ${err}. Please try again.`
+          );
+        });
+    }
+  });
 }
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    el.toTopButton.style.display = 'block';
+  } else {
+    el.toTopButton.style.display = 'none';
+  }
+});
+
+el.toTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
--- src/refs/index.js
+++ src/refs/index.js
@@ -1,7 +1,9 @@
 const el = {
   form: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
-  moreButton: document.querySelector('.load-more'),
+  // moreButton: document.querySelector('.load-more'),
+  guard: document.querySelector('.js-guard'),
+  toTopButton: document.querySelector('.to-top-button'),
 };
 
 export { el };
